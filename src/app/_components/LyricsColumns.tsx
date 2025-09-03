2b6d3796cbc5f40f05a3ae77e8b3b10626f74fbf
Rebakure <kevinrebakure@gmail.com>
2025-09-03 12:25:34 +0200

    feat: set up some ui structure

---
+++ src/app/_components/LyricsColumns.tsx
@@ -0,0 +1,185 @@
+import { MicVocal } from "lucide-react";
+import cn from "../utils/cn";
+
+type Lyrics = {
+  type: "chorus" | "verse" | "bridge" | "title";
+  content: string;
+};
+function LyricsColumns() {
+  return (
+    <div className="col-span-3 grid grid-cols-2 w-[1000px] mx-auto gap-12">
+      <div className="space-y-16">
+        {originalLyrics.length > 0 &&
+          originalLyrics.map((lyric, i) => (
+            <div key={lyric.type + i}>
+              <p
+                className={cn(
+                  "text-2xl leading-12",
+                  lyric.type === "title" && "text-5xl font-bold",
+                  lyric.type === "chorus" && "pl-10"
+                )}
+              >
+                {lyric.content}
+              </p>
+            </div>
+          ))}
+        {originalLyrics.length === 0 && (
+          <div className="flex flex-col items-center justify-center">
+            <MicVocal size={128} strokeWidth={1} />
+            <p className="text-2xl mt-10">Search for a song</p>
+          </div>
+        )}
+      </div>
+      <div className="space-y-16">
+        {translatedLyrics.map((lyric, i) => (
+          <div key={lyric.type + i}>
+            <p
+              className={cn(
+                "text-2xl leading-12",
+                lyric.type === "title" && "text-5xl font-bold",
+                lyric.type === "chorus" && "pl-10"
+              )}
+            >
+              {lyric.content}
+            </p>
+          </div>
+        ))}
+        {translatedLyrics.length === 0 && null}
+      </div>
+    </div>
+  );
+}
+
+// const originalLyrics: Lyrics[] = [];
+
+const originalLyrics: Lyrics[] = [
+  {
+    type: "title",
+    content: `A toi`,
+  },
+  {
+    type: "verse",
+    content: `A toi, à la façon que tu as d'être belle
+A la façon que tu as d'être à moi
+A tes mots tendres un peu artificiels quelquefois
+A toi, à la petite fille que tu étais
+A celle que tu es encore souvent
+A ton passé, à tes secrets
+A tes anciens princes charmants`,
+  },
+  {
+    type: "chorus",
+    content: `A la vie, à l'amour
+A nos nuits, à nos jours
+A l'éternel retour de la chance
+A l'enfant qui viendra
+Qui nous ressemblera
+Qui sera à la fois toi et moi`,
+  },
+  {
+    type: "verse",
+    content: `A moi, à la folie dont tu es la raison
+A mes colères sans savoir pourquoi
+A mes silences et à mes trahisons quelquefois
+A moi, au temps que j'ai passé à te chercher
+Aux qualités dont tu te moques bien
+Aux défauts que je t'ai cachés
+A mes idées de baladin`,
+  },
+  {
+    type: "chorus",
+    content: `A la vie, à l'amour
+A nos nuits, à nos jours
+A l'éternel retour de la chance
+A l'enfant qui viendra
+Qui nous ressemblera
+Qui sera à la fois toi et moi`,
+  },
+  {
+    type: "bridge",
+    content: `A nous, aux souvenirs que nous allons nous faire
+A l'avenir et au présent surtout
+A la santé de cette vieille terre qui s'en fout
+A nous, à nos espoirs et à nos illusions
+A notre prochain premier rendez-vous
+A la santé de ces millions d'amoureux
+Qui sont comme nous`,
+  },
+  {
+    type: "verse",
+    content: `A toi, à la façon que tu as d'être belle
+A la façon que tu as d'être à moi
+A tes mots tendres un peu artificiels quelquefois
+A toi, à la petite fille que tu étais
+A celle que tu es encore souvent
+A ton passé, à tes secrets
+A tes anciens princes charmants`,
+  },
+];
+
+const translatedLyrics: Lyrics[] = [
+  {
+    type: "title",
+    content: `To you`,
+  },
+  {
+    type: "verse",
+    content: `To you, to the way you are beautiful
+To the way you belong to me
+To your tender words, sometimes a little artificial
+To you, to the little girl you once were
+To the one you still often are
+To your past, to your secrets
+To your former charming princes`,
+  },
+  {
+    type: "chorus",
+    content: `To life, to love
+To our nights, to our days
+To the eternal return of luck
+To the child who will come
+Who will look like us
+Who will be both you and me`,
+  },
+  {
+    type: "verse",
+    content: `To me, to the madness for which you are the reason
+To my angers without knowing why
+To my silences and sometimes my betrayals
+To me, to the time I spent searching for you
+To the qualities you laugh at
+To the flaws I hid from you
+To my minstrel-like ideas`,
+  },
+  {
+    type: "chorus",
+    content: `To life, to love
+To our nights, to our days
+To the eternal return of luck
+To the child who will come
+Who will look like us
+Who will be both you and me`,
+  },
+  {
+    type: "bridge",
+    content: `To us, to the memories we will create
+To the future and above all the present
+To the health of this old earth that doesn’t care
+To us, to our hopes and our illusions
+To our next first date
+To the health of those millions of lovers
+Who are like us`,
+  },
+  {
+    type: "verse",
+    content: `To you, to the way you are beautiful
+To the way you belong to me
+To your tender words, sometimes a little artificial
+To you, to the little girl you once were
+To the one you still often are
+To your past, to your secrets
+To your former charming princes`,
+  },
+];
+
+export default LyricsColumns;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,48 @@
-import Image from "next/image";
+"use client";
+
+import { Music } from "lucide-react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import LyricsColumns from "./_components/LyricsColumns";
+
+type Inputs = {
+  search: string;
+};
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <div className="p-8">
+      <h1 className="text-5xl mb-12">understand it.</h1>
+
+      <div className="grid grid-cols-4">
+        <div>
+          <form action="" onSubmit={handleSubmit(onSubmit)}>
+            <label className="input">
+              <Music size={24} />
+              <input
+                {...register("search")}
+                type="text"
+                className="grow"
+                placeholder="Search for a song"
+              />
+            </label>
+            <button className="btn btn-active mt-3">Search</button>
+          </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <LyricsColumns />
+      </div>
     </div>
   );
 }
