96492e4cea27a9dc6a26cbb1dc0428aa4021f111
Rebakure <kevinrebakure@gmail.com>
2025-09-24 07:58:06 +0200

    refactor: clean up the components

---
+++ src/_components/lyricsB.ts
@@ -0,0 +1,40 @@
+import { Lyrics } from "./LyricsColumns";
+
+const lyricsB: Lyrics = [
+  {
+    type: "title",
+    content: "To You",
+  },
+  {
+    type: "verse",
+    content:
+      "To you, to the way you are beautiful\nTo the way you belong to me\nTo your tender words, sometimes a little artificial\nTo you, to the little girl you once were\nTo the one you still are so often\nTo your past, to your secrets\nTo your former charming princes",
+  },
+  {
+    type: "chorus",
+    content:
+      "To life, to love\nTo our nights, to our days\nTo the eternal return of luck\nTo the child who will come\nWho will resemble us\nWho will be both you and me",
+  },
+  {
+    type: "verse",
+    content:
+      "To me, to the madness for which you are the reason\nTo my angers without knowing why\nTo my silences and sometimes my betrayals\nTo me, to the time I spent looking for you\nTo the qualities you laugh about\nTo the flaws I’ve hidden from you\nTo my wandering minstrel ideas",
+  },
+  {
+    type: "chorus",
+    content:
+      "To life, to love\nTo our nights, to our days\nTo the eternal return of luck\nTo the child who will come\nWho will resemble us\nWho will be both you and me",
+  },
+  {
+    type: "bridge",
+    content:
+      "To us, to the memories we will create together\nTo the future and above all the present\nTo the health of this old earth that doesn’t care\nTo us, to our hopes and illusions\nTo our next first date\nTo the health of those millions of lovers\nWho are like us",
+  },
+  {
+    type: "verse",
+    content:
+      "To you, to the way you are beautiful\nTo the way you belong to me\nTo your tender words, sometimes a little artificial\nTo you, to the little girl you once were\nTo the one you still are so often\nTo your past, to your secrets\nTo your former charming princes",
+  },
+];
+
+export default lyricsB;
